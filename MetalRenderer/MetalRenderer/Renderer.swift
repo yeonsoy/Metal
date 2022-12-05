a52ec89c5bbb4dae62496f976f60a734ad043bd3
USER <user@AL02349814.local>
2022-12-05 23:24:24 +0900

    Set Up Metal in Swift

---
+++ MetalRenderer/MetalRenderer/Renderer.swift
@@ -0,0 +1,65 @@
+//
+//  Renderer.swift
+//  MetalRenderer
+//
+//  Created by USER on 2022/12/05.
+//
+
+import Foundation
+import MetalKit
+
+class Renderer: NSObject {
+    static var device: MTLDevice!
+    let commandQueue: MTLCommandQueue
+    static var library: MTLLibrary!
+    let pipelineState: MTLRenderPipelineState
+    
+    init(view: MTKView) {
+        guard let device = MTLCreateSystemDefaultDevice(),
+              let commandQueue = device.makeCommandQueue() else {
+            fatalError("Unable to connect to GPU")
+        }
+        Renderer.device = device
+        self.commandQueue = commandQueue
+        Renderer.library = device.makeDefaultLibrary()!
+        pipelineState = Renderer.createPipelineState()
+        super.init()
+    }
+    
+    static func createPipelineState() -> MTLRenderPipelineState {
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        
+        // pipeline state properties
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        let vertexFunction = Renderer.library.makeFunction(name: "vertex_main")
+        let fragmentFunction = Renderer.library.makeFunction(name: "fragment_main")
+        pipelineStateDescriptor.vertexFunction = vertexFunction
+        pipelineStateDescriptor.fragmentFunction = fragmentFunction
+        
+        return try! Renderer.device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+    }
+}
+
+extension Renderer: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+        guard let commandBuffer = commandQueue.makeCommandBuffer(),
+              let drawable = view.currentDrawable,
+              let descriptor = view.currentRenderPassDescriptor,
+              let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor) else {
+            return
+        }
+        
+        commandEncoder.setRenderPipelineState(pipelineState)
+        
+        // draw call
+        commandEncoder.drawPrimitives(type: .point, vertexStart: 0, vertexCount: 1)
+        commandEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+}
