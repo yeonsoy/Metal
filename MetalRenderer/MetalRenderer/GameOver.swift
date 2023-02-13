985a08acc670d2aa52a83f8f76614860ebc31bf2
leesy <yeondiary@gmail.com>
2023-02-14 00:17:47 +0900

    Challenge: Game Over

--- MetalRenderer/MetalRenderer/GameOver.swift
+++ MetalRenderer/MetalRenderer/GameOver.swift
@@ -8,14 +8,30 @@
 import Foundation
 
 class GameOver: Scene {
+    var messageModel: Model?
+    
     var win = false {
         didSet {
-            print("GAME OVER -", win ? "You Won" : "You Lost")
+            if win {
+                messageModel = Model(name: "youwin")
+            } else {
+                messageModel = Model(name: "youlose")
+            }
+            add(node: messageModel!)
         }
     }
     
-    override func click(location: float2) {
+    override func click(location: SIMD2<Float>) {
         let scene = RayBreak(sceneSize: sceneSize)
         sceneDelegate?.transition(to: scene)
+    }
+    
+    override func setupScene() {
+        camera.distance = 15
+        camera.fov = radians(fromDegrees: 100)
+    }
+    
+    override func updateScene(deltaTime: Float) {
+        messageModel?.rotation.y += .pi / 4 * deltaTime
     }
 }
